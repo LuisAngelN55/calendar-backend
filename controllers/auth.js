d835f8e1fe9471a47a7cbf1540d321ca378adcd7
LuisMoov <lang@zuntrix.com>
2022-09-14 17:18:27 -0500

    374. Custom Middlewares

---
+++ controllers/auth.js
@@ -0,0 +1,42 @@
+const { response } = require('express');
+
+
+const createUser = (req, res = response ) => {
+
+    const { name, email, password } = req.body;
+
+    //Resultado correcto
+    res.status(201).json({
+        ok: true,
+        msg: 'registro',
+        name,
+        email,
+        password
+    })
+}
+
+const loginUser = (req, res = response ) => {
+
+    const { email, password } = req.body;
+
+    //Resultado correcto
+    res.json({
+        ok: true,
+        msg: 'login',
+        email,
+        password
+    })
+}
+
+const renewToken = (req, res = response ) => {
+    res.json({
+        ok: true,
+        msg: 'renew'
+    })
+}
+
+module.exports = {
+    createUser,
+    loginUser,
+    renewToken,
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('dotenv').config();
+
+// Muestra todas las variables de entorno incluida las variables del .env
+// console.log( process.env );
+
+//* Crear el servidor de express
+const app = express();
+
+
+//* Directorio público
+app.use( express.static('public') );
+
+
+//* Lectura y parseo del body
+app.use( express.json() );
+
+//* Rutas
+app.use('/api/auth', require('./routes/auth'));
+
+
+//* Escuchar peticiones
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
